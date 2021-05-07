d5f0f62c8078529a865b4b8e5737f394dd04e4da
jsalgg <mzootfb@Lukes-MBP.attlocal.net>
2021-05-07 12:41:39 -0400

    quick debugging

--- routes/questions.js
+++ routes/questions.js
@@ -1,86 +1,108 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs');
-const db = require('../db/models')
-const { User, Question, Answer, Vote } = db
-const { csrfProtection, asyncHandler } = require('./utils');
-const { loginUser, logoutUser, requireAuth } = require('../auth');
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const db = require("../db/models");
+const { User, Question, Answer, Vote } = db;
+const { csrfProtection, asyncHandler } = require("./utils");
+const { loginUser, logoutUser, requireAuth } = require("../auth");
 
 const questionValidators = [
-    check('title')
-        .exists({ checkFalsy: true })
-        .withMessage('Please provide a value for the question title')
-        .isLength({ max: 150 })
-        .withMessage('Title must not be more than 50 characters long'),
-    check('query')
-        .exists({ checkFalsy: true })
-        .withMessage('Please provide a value for the content of the question')
-]
+  check("title")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for the question title")
+    .isLength({ max: 150 })
+    .withMessage("Title must not be more than 50 characters long"),
+  check("query")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for the content of the question"),
+];
 
-router.get('/questions', csrfProtection, (req, res) => {
-    res.render('new-question', {
-        csrfToken: req.csrfToken(),
-    })
-})
+router.get("/questions", csrfProtection, (req, res) => {
+  res.render("new-question", {
+    csrfToken: req.csrfToken(),
+  });
+});
 
-router.post('/questions', csrfProtection, questionValidators , asyncHandler(async (req, res) => {
+router.post(
+  "/questions",
+  csrfProtection,
+  questionValidators,
+  asyncHandler(async (req, res) => {
     const { title, query } = req.body;
-    const question = Question.build({
+    let question;
+    if (!req.session.auth) {
+      res.render("user-login", {
+        title: "Must be logged in to post question",
+      });
+    } else {
+      question = Question.build({
         ownerId: req.session.auth.userId,
         title,
         query,
-    });
+      });
+    }
     const validatorErrors = validationResult(req);
 
     if (validatorErrors.isEmpty()) {
-        await question.save();
-        res.redirect(`/questions/${question.id}`)
+      await question.save();
+      res.redirect(`/questions/${question.id}`);
     }
     const errors = validatorErrors.array().map((error) => error.msg);
     res.render("new-question", {
-        title,
-        query,
-        question,
-        errors,
-        csrfToken: req.csrfToken(),
+      title,
+      query,
+      question,
+      errors,
+      csrfToken: req.csrfToken(),
     });
-}));
+  })
+);
 
-router.get('/questions/:id(\\d+)', csrfProtection, asyncHandler(async (req, res) => {
+router.get(
+  "/questions/:id(\\d+)",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
     const questionId = parseInt(req.params.id, 10);
     const question = await Question.findByPk(questionId, {
-        include: [User, Answer]
-    });
-    
-    
-    const answers = await Answer.findAll({
-        where: {
-            questionId: question.id
-        },
-        include: [User, Vote]
+      include: [User, Answer],
     });
 
-    const abr = req.session.auth.userId
-    const isUserLoggedIn = (abr === question.ownerId) ? true : false
-    const isOwner = (abr === question.ownerId) ? true : false
+    const answers = await Answer.findAll({
+      where: {
+        questionId: question.id,
+      },
+      include: [User, Vote],
+    });
+
+    let abr;
+    if (req.session.auth) {
+      abr = req.session.auth.user.userId;
+    } else {
+      abr = User.build({});
+    }
+    const isUserLoggedIn = abr === question.ownerId ? true : false;
+    const isOwner = abr === question.ownerId ? true : false;
 
     // const isUserAnswerLoggedIn = (abr === question.Answer.ownerId) ? true : false
     // const isAnswersOwner = (abr === question.Answer.ownerId) ? true : false
 
-
-    res.render('single-question', {
-        title: question.title,
-        abr,
-        answers,
-        question,
-        isUserLoggedIn,
-        isOwner,
-        csrfToken: req.csrfToken(),
+    res.render("single-question", {
+      title: question.title,
+      abr,
+      answers,
+      question,
+      isUserLoggedIn,
+      isOwner,
+      csrfToken: req.csrfToken(),
     });
-}));
+  })
+);
 
-router.post('/questions/:id/edit', requireAuth, asyncHandler(async (req, res) => {
+router.post(
+  "/questions/:id/edit",
+  requireAuth,
+  asyncHandler(async (req, res) => {
     const questionId = parseInt(req.params.id, 10);
     const question = await Question.findByPk(questionId);
     await question.update({ title: req.body.title, query: req.body.query });
@@ -89,19 +111,25 @@ router.post('/questions/:id/edit', requireAuth, asyncHandler(async (req, res) =>
     //     question
     // })
     res.redirect(`/questions/${questionId}`);
-}));
+  })
+);
 
-router.get('/questions/:id/edit', csrfProtection, asyncHandler(async (req, res) => {
+router.get(
+  "/questions/:id/edit",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
     const questionId = parseInt(req.params.id, 10);
     const question = await Question.findByPk(questionId);
-    const isUserLoggedIn = (req.session.auth.userId === question.ownerId) ? true : false
-    res.render('edit-question', {
-        title: question.title,
-        isUserLoggedIn,
-        question,
-        csrfToken: req.csrfToken(),
-    })
-}))
+    const isUserLoggedIn =
+      req.session.auth.userId === question.ownerId ? true : false;
+    res.render("edit-question", {
+      title: question.title,
+      isUserLoggedIn,
+      question,
+      csrfToken: req.csrfToken(),
+    });
+  })
+);
 
 // router.get('/questions/:id/delete', asyncHandler(async (req, res) => {
 //     const question = await Question.findByPk(req.params.id);
@@ -113,17 +141,17 @@ router.get('/questions/:id/edit', csrfProtection, asyncHandler(async (req, res) 
 //         alert('This is not working')
 //     }
 
-
 // }))
-router.post('/questions/:id/delete', requireAuth, asyncHandler(async (req, res) => {
-
-    const id = parseInt(req.params.id, 10)
+router.post(
+  "/questions/:id/delete",
+  requireAuth,
+  asyncHandler(async (req, res) => {
+    const id = parseInt(req.params.id, 10);
     const question = await Question.findByPk(id);
 
     await question.destroy();
-    res.redirect('/');
-    
-
-}));
+    res.redirect("/");
+  })
+);
 
 module.exports = router;
