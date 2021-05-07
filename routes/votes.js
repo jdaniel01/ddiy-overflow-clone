425e6c411b58f14c2a0a5cf610d2df51ea883f79
Jefferson <jeffersongarcia1599@gmail.com>
2021-05-07 17:46:45 -0500

    Fixing a bug with length

--- routes/votes.js
+++ routes/votes.js
@@ -7,40 +7,90 @@ const { User, Question, Answer, Vote } = db;
 const { csrfProtection, asyncHandler } = require("./utils");
 const { loginUser, logoutUser, requireAuth } = require("../auth");
 
-router.post('/questions/:id/answer/answerId', csrfProtection, asyncHandler(async (req, res) => {
+router.get('/questions/:id/answer/:answerId/upvote', csrfProtection, asyncHandler(async (req, res) => {
     const answerId = req.params.answerId;
-    const userId = req.session.auth.userId;
+    const ownerId = req.session.auth.userId;
     const vote = await Vote.findOne({
-        where: answerId, userId
+        where: {
+            answerId, ownerId
+        }
     })
-    let upAndDownVote;
-
-    if (vote) {
-        upAndDownVote = Vote.build({
-            userId,
-            answerId
+    let upVote;
+    console.log('This is value of vote', vote)
+    console.log('This is value of vote', answerId)
+    console.log('This is value of vote', ownerId)
+    if (!vote) {
+        upVote = Vote.build({
+            ownerId,
+            answerId,
+            value: true,
         });
-        await upAndDownVote.save()
+        await upVote.save()
     }
     else {
         await vote.destroy();
     }
 
-    const votes = await Vote.findAll({
-        where: {
-            answerId
-        }
-    })
-
+    // const votes = await Vote.findAll({
+    //     where: {
+    //         answerId
+    //     }
+    // })
+    console.log("Here we are***********")
     res.render('votes', {
         vote,
-        votes,
-        upAndDownVote,
+        upVote,
         csrfToken: req.csrfToken()
     })
 }))
 
+router.get('/questions/:id/answer/:answerId/downvote', csrfProtection, asyncHandler(async (req, res) => {
+    const answerId = req.params.answerId;
+    const ownerId = req.session.auth.userId;
+    const questionId = parseInt(req.params.id, 10);
+    const question = await Question.findByPk(questionId, {
+      include: [User, Answer],
+    });
+    const vote = await Vote.findOne({
+        where: {
+            answerId, ownerId
+        }
+    })
+    let downVote;
 
+    if (!vote) {
+        downVote = Vote.build({
+            userId,
+            answerId,
+            value: false,
+        });
+        await downVote.save()
+    }
+    else {
+        await vote.destroy();
+    }
+
+    // const votes = await Vote.findAll({
+    //     where: {
+    //         answerId
+    //     }
+    // })
+    const answers = await Answer.findAll({
+        where: {
+          questionId: question.id,
+        },
+        include: [User, Vote],
+      });
+
+
+    res.render('votes', {
+        vote,
+        downVote,
+        question,
+        answers,
+        csrfToken: req.csrfToken()
+    })
+}))
 
 
 module.exports = router;
