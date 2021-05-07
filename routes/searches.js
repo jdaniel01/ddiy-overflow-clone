c9247fe6f27579b024bc47f897c3caeb31685d59
jdaniel01 <professor4cob@outlook.com>
2021-05-07 09:46:53 -0700

    finishing regex

--- routes/searches.js
+++ routes/searches.js
@@ -4,68 +4,71 @@ const { User, Question, Answer } = require('../db/models');
 const { csrfProtection, asyncHandler } = require('./utils');
 const { Op } = require('sequelize');
 
+const makeRegex = (array) => {
+    let regExps = ""
+    for (let i = 0; i < array.length; i++) {
+        regExps += `\\/${array[0]}\\:\\w*$/`
+    }
+    return regExps;
+}
+
 router.post('/searches', asyncHandler(async (req, res) => {
     const { filter } = req.body;
-    let filters = filter.split(' ' || ', ' || ",");
+    let filters = filter.split(' ');
 
     let results = [];
     let searchRegex;
-
+    let searchRegexArray;
 
     if (filters.length) {
         if (filters.length === 1 && (filters[0] !== "")) {
             searchRegex = new RegExp(filters[0])
-        } else if (filters[0] !== "") {
-            searchRegex = makeRegex(filters);
+        } else if (filters.length > 1 && filters[0] !== "") {
+            searchRegex = new RegExp(makeRegex(filters));
         }
     }
 
-
-    if (!searchWords.length) {
-        res.render('searches', { title: "Search Results", errors: ["You must provide search criteria."] })
-    } else {
-
-        await searchWords.forEach(async word => {
-
-            let questions = await Question.findAll({
-                where: {
-                    [Op.or]: [
-                        { title: { [Op.startsWith]: word } },
-                        { title: { [Op.endsWith]: word } },
-                        { title: { [Op.substring]: word } },
-                        { title: { [Op.iLike]: word } },
-                        { query: { [Op.startsWith]: word } },
-                        { query: { [Op.endsWith]: word } },
-                        { query: { [Op.substring]: word } },
-                        { query: { [Op.iLike]: word } }
-                    ]
-                }
-            });
-            if (questions.length) {
-                results.push(...questions)
+    if (searchRegex) {
+        res.send(searchRegex);
+        let questions = await Question.findAll({
+            where: {
+                [Op.or]: [
+                    { title: { [Op.startsWith]: searchRegex } },
+                    { title: { [Op.endsWith]: searchRegex } },
+                    { title: { [Op.substring]: searchRegex } },
+                    { title: { [Op.iLike]: searchRegex } },
+                    { query: { [Op.startsWith]: searchRegex } },
+                    { query: { [Op.endsWith]: searchRegex } },
+                    { query: { [Op.substring]: searchRegex } },
+                    { query: { [Op.iLike]: searchRegex } }
+                ]
             }
-            let answers = await Answer.findAll({
-                where: {
-                    [Op.or]: [
-                        { answer: { [Op.startsWith]: word } },
-                        { answer: { [Op.endsWith]: word } },
-                        { answer: { [Op.substring]: word } },
-                        { answer: { [Op.iLike]: word } },
-                        { content: { [Op.startsWith]: word } },
-                        { content: { [Op.endsWith]: word } },
-                        { content: { [Op.substring]: word } },
-                        { content: { [Op.iLike]: word } }
-                    ]
-                }
-            });
-            if (answers.length) {
-                results.push(...answers)
+        });
+        if (questions.length) {
+            results.push(...questions)
+        }
+        let answers = await Answer.findAll({
+            where: {
+                [Op.or]: [
+                    { answer: { [Op.startsWith]: searchRegex } },
+                    { answer: { [Op.endsWith]: searchRegex } },
+                    { answer: { [Op.substring]: searchRegex } },
+                    { answer: { [Op.iLike]: searchRegex } },
+                    { content: { [Op.startsWith]: searchRegex } },
+                    { content: { [Op.endsWith]: searchRegex } },
+                    { content: { [Op.substring]: searchRegex } },
+                    { content: { [Op.iLike]: searchRegex } }
+                ]
             }
-            // res.render(answers);//('searches', {title: "Search Results", results});
-            console.log(results);
-            res.send(results);
-        })
+        });
+        if (answers.length) {
+            results.push(...answers)
+        }
+        // res.render(answers);//('searches', {title: "Search Results", results});
+        console.log(results);
+        res.send(results);
     }
+
 
 
 }));
