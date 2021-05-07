0717c98543932466448f3c2e033cd9723f87e3fc
jsalgg <mzootfb@Lukes-MBP.attlocal.net>
2021-05-07 19:02:05 -0400

    debigging stuff really

---
+++ public/javascripts/filtersIndex.js
@@ -0,0 +1,31 @@
+let newest;
+let oldest;
+let mostAnswered;
+let span;
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
+  span = document.getElementById("filter");
+  newest = document.querySelector(".index-question--outer0");
+  oldest = document.querySelector(".index-question--outer1");
+  mostAnswered = document.querySelector(".index-question--outer2");
+});
+var switchDisplay = (option) => {
+  console.log(newest);
+  if (option === "newest") {
+    span.innerHTML = "Newest" + " Questions";
+    oldest.hidden = true;
+    mostAnswered.hidden = true;
+    newest.hidden = false;
+  } else if (option === "oldest") {
+    span.innerHTML = "Oldest" + " Questions";
+    oldest.hidden = false;
+    mostAnswered.hidden = true;
+    newest.hidden = true;
+  } else if (option === "mostAnswered") {
+    span.innerHTML = "Most Answered" + " Questions";
+    oldest.hidden = true;
+    mostAnswered.hidden = false;
+    newest.hidden = true;
+  } else {
+  }
+};
